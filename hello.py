8040c1e5f2d2fe78790d4d428114439fb1ded480
Fylwind <fyl@wolfpa.ws>
2014-08-01 01:42:34 -0400

    Strip slashes from URLs

--- hello.py
+++ hello.py
@@ -1,8 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, request, url_for
 
 app = Flask(__name__)
+if os.path.exists("DEBUG"):
+    app.debug = True
+app.url_map.strict_slashes = False
+
+@app.before_request
+def remove_trailing_slash():
+    if len(request.path) != 1 and request.path.endswith('/'):
+        return redirect(request.path[:-1])
 
 @app.route('/')
-def hello():
-    return 'Yay!'
+def main():
+    return 'Woot, it works!'
+
+@app.route('/facts')
+def dummy():
+    return 'Nevy secretly likes green~ :3'
